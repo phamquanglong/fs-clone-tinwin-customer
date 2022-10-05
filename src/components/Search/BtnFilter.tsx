678ef2fd2683b5772723dc6e3157b8d7096df6ad
tuyetna-comartek <tuyetna@comartek.com>
2022-10-05 16:00:10 +0700

    fix UI

--- src/components/Search/BtnFilter.tsx
+++ src/components/Search/BtnFilter.tsx
@@ -57,28 +57,6 @@ const BtnFilter: React.FC<Props> = props => {
 
     return (
         <View>
-            {/* <View className={`flex-row px-6 mt-6 `}>
-                {label.map(item => (
-                    <View key={item.id} >
-                        <TouchableOpacity
-                            className={`w-28 items-center border-b-2 ${status === item.title ? 'border-b-[#FC832D]' : 'border-b-[#48484A]'
-                                } `}
-                            key={item.id}
-                            onPress={() => {
-                                setStatusFilter(item.title);
-                            }}>
-                            <Text
-                                className={`mb-3 ${status === item.title ? 'text-[#FC832D]' : 'text-[#48484A]'
-                                    }`}>
-                                {item.title}
-                            </Text>
-                        </TouchableOpacity>
-
-                    </View>
-
-
-                ))}
-            </View> */}
             <View className={`px-6 mt-6`}>
                 <FlatList data={label} renderItem={renderItem} horizontal />
             </View>
